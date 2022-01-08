6af449e500d3a4887a77d8df948d7f32786048e1
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2022-01-08 18:56:07 +0100

    More translations

--- room/src/index.ts
+++ room/src/index.ts
@@ -1,6 +1,7 @@
 import en from './messages.json'
+import no from '../dist/messages.no.json'
 export const defaultLocale = 'en'
-export const locales = { en }
+export const locales = { en, no }
 
 const _localeFiles: { [ key: string ]: string } = {}
 Object.keys(locales).forEach((value: string) => {
